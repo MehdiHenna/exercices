c35df527306691f3f9b9729b9d26ac19653d142a
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-10 17:07:36 +0100

    troisieme journée X

--- projet/tudo/app.js
+++ projet/tudo/app.js
@@ -1,23 +1,47 @@
-"use strict"
+
 $(document).ready(function(){
 	
 	$(document).on('click', '#checkOn', function(){
+		
 		var contenu = $('input[name=rechercher]').val();
 
 		
 			$('#todoshermanos').append('<li class="description hello"><input type="checkbox" class="check"/>' + contenu + '</li>');
 
-		
+
 
 	});
 
-	
+	$(document).on('keypress', function(event){
+
+		var contenu = $('input[name=rechercher]').val();
+
+		if (event.which === 13) {
+
+			
+
+		
+			$('#todoshermanos').append('<li class="description hello"><input type="checkbox" class="check"/>' + contenu + '</i>');
+
+
+		}
+
+		if (contenu == false) {
+
+			alert("Il faut ecrire quelque chose.");
+
+		}
+
+	});
 
 	$(document).on('click', '[type=checkbox]', function(){
-
-		$('li').toggleClass("java");
-
-	});
-
+		
+		$(this).parent('li').toggleClass("java");
 
 	});
+
+
+});
+
+
+
